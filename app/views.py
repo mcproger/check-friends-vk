f610ea0b75904e61e5544fb2f67261ec650be385
mcproger <artem7lomonosov@gmail.com>
2017-03-22 23:33:09 +0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -5,28 +5,40 @@ from flask import request
 import requests
 
 
-def input():
-    return request.args.get('text')
-
-
 @app.route('/')
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     return render_template('login.html',
-        title = 'Sign In',
-        form = form)
+        title='Check VK Friends',
+        form=form)
+
+
+def user_id_input():
+    return request.args.get('text')#id may be in text format
+
+
+def get_id_in_number_format():
+    user_ids = user_id_input()
+    param = {'user_ids': user_ids}
+    id = requests.get('https://api.vk.com/method/users.get', params=param).json()
+    if 'error' in id:
+        error_code = id['error']['error_code']
+        error = errors(error_code)
+        return render_template('index.html', error=error)
+    id = requests.get('https://api.vk.com/method/users.get', params=param).json()['response'][0]['uid']
+    return id
 
 
 @app.route('/friends_online')
 def check_online_friends():
-    user_ids = input()
-    if user_ids == None:
-        return render_template('login.html')
+    user_ids = user_id_input()
     param = {'user_ids': user_ids}
     id = requests.get('https://api.vk.com/method/users.get', params=param).json()
     if 'error' in id:
-        return 'Ошибка'
+        error_code = id['error']['error_code']
+        error = errors(error_code)
+        return render_template('login.html', error=error)
     id = requests.get('https://api.vk.com/method/users.get', params=param).json()['response'][0]['uid']
     params = {'user_id': id, 'fields': 'online', 'version': '5.62'}
     friends = requests.get('https://api.vk.com/method/friends.get', params=params).json()['response']
@@ -39,13 +51,14 @@ def check_online_friends():
 
 def errors(error_code):
     if error_code == 6:
-        return 'Слишком много запросов'
-    if error_code == 8:
-        return 'Неверный запрос'
+        return 'Too many requests'
+    if error_code == 113:
+        return 'Invalid input'
     if error_code == 15:
-        return 'Доступ запрещен'
+        return 'Access closed'
     if error_code == 200:
-        return 'Доступ запрещен'
+        return 'Access closed'
+
 
 
 
