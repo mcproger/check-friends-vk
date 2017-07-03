d8c8c8ff6f0cdf8adf68f197a58173f79d564e7c
mcproger <artem7lomonosov@gmail.com>
2017-07-03 17:18:50 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 #!flask/bin/python
 #!flask/bin/python
 from app import app
-app.run(debug = True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
